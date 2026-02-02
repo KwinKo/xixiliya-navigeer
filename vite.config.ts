0acc79c3f6ffa7b41343ceb1c8285db75197c8eb
KwinKo <68121338+KwinKo@users.noreply.github.com>
2026-02-03 01:37:13 +0800

    Add vite.config.ts configuration file

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  server: {
+    allowedHosts: ['frp-fun.com', 'nodo4:4045'],
+    host: true,
+    port: 5174
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
