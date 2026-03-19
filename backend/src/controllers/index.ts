3f5e7af1eca98251fa704fec1272a86ed7aaf9b2
KwinKo <68121338+KwinKo@users.noreply.github.com>
2026-03-19 14:26:23 -0400

    Delete backend directory

--- backend/src/controllers/index.ts
+++
@@ -1,7 +0,0 @@
-// Export all controllers
-export * from './auth.controller';
-export * from './user.controller';
-export * from './bookmark.controller';
-export * from './category.controller';
-export * from './data.controller';
-export * from './admin.controller';
